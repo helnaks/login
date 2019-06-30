841d20d91ea891f9758ee8e3ba057b9f7f440356
helnaks <helna@e.pinetech.in>
2019-06-30 15:40:55 +0530

    Email validation and user validation functions

--- UserModel.php
+++ UserModel.php
@@ -22,4 +22,46 @@ class UserModel
 
         return mysqli_query($this->dbConnection->connection, $sql);
     }
+
+    /**
+     * Validate user
+     *
+     * @param array $data
+     * @return void
+     */
+    function validateUser(array $data)
+    {
+        $sql = "SELECT id, email, password FROM user WHERE email = '" . $data['email'] . "'";
+       
+        $result = mysqli_query($this->dbConnection->connection, $sql);  
+            
+        if (mysqli_num_rows($result) == 1) {
+            $userData = mysqli_fetch_array($result, MYSQLI_ASSOC);
+            if (password_verify($data['password'], $userData['password'])) {
+                return $userData['id'];
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Check for existing email id
+     *
+     * @param string $email
+     * @return bool
+     */
+    function validteEmail(string $email) {
+        $sql = "SELECT id FROM user WHERE email = '" . $email . "'";
+       
+        $result = mysqli_query($this->dbConnection->connection, $sql);  
+            
+        if (mysqli_num_rows($result) > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
